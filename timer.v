d5d83917db187d78e09535e849ce24a3b3d73812
Ainski <2351579@tongji.edu.cn>
2025-11-17 22:33:06 +0800

    .

--- timer.v
+++ timer.v
@@ -20,8 +20,90 @@ reg buzy_n_r_last;
 reg buzy_n_r;
 reg [`width-1:0] sec_r,min_r,hour_r;
 reg up_r;
-assign alarm=(~buzy_n_r_last&buzy_n_r)&
-  ((up_r&(sec_r==sec_set)&(min_r==min_set)&(hour_r==hour_set))|
+assign alarm=((up_r&(sec_r==sec_set)&(min_r==min_set)&(hour_r==hour_set)&(sec_r!=0)&(min_r!=0)&(hour_r!=0))|
     (!up_r&(sec_r==0)&(min_r==0)&(hour_r==0)));
-  
+assign sec=sec_r;
+assign min=min_r;
+assign hour=hour_r;
+assign buzy_n=buzy_n_r;
+always @(posedge clk) begin
+  if(cut_n) begin
+    up_r<=0;
+    sec_set<=0;
+    min_set<=0;
+    hour_set<=0;
+  end else begin
+    if (write&&buzy_n_r)begin
+      buzy_n_r<=0;
+      sec_set<=insec;
+      min_set<=inmin;
+      hour_set<=inhour;
+      up_r<=up;
+    end else begin
+      sec_set<=sec_set;
+      min_set<=min_set;
+      hour_set<=hour_set;
+      up_r<=up_r;
+    end
+  end
+end
+always @ (posedge clk) begin
+  if(cut_n||alarm) begin
+    buzy_n_r<=1;
+    buzy_n_r_last<=buzy_n_r;
+  end else if(start) begin
+    buzy_n_r<=0;
+    buzy_n_r_last<=buzy_n_r;
+  end else begin
+    buzy_n_r<=buzy_n_r;
+    buzy_n_r_last<=buzy_n_r_last;
+  end
+end
+always @ (posedge clk) begin
+  if(cut_n||buzy_n_r||alarm) begin
+    if(up_r) begin
+      sec_r<=0;
+      min_r<=0;
+      hour_r<=0;
+    end else begin
+      sec_r<=sec_set;
+      min_r<=min_set;
+      hour_r<=hour_set;
+    end
+  end else begin
+    if(up_r) begin
+      if(sec_r==59)begin
+        if(min_r==59)begin
+          hour_r<=hour_r+1;
+          min_r<=0;
+          sec_r<=0;
+        end else begin
+          hour_r<=hour_r;
+          min_r<=min_r+1;
+          sec_r<=0;
+        end
+      end else begin
+        hour_r<=hour_r;
+        min_r<=min_r;
+        sec_r<=sec_r+1;
+      end
+    end else begin
+      if(sec_r==0)begin
+        if(min_r==0)begin
+          hour_r<=hour_r-1;
+          min_r<=59;
+          sec_r<=59;
+        end else begin
+          hour_r<=hour_r;
+          min_r<=min_r-1;
+          sec_r<=59;
+        end
+      end else begin
+        hour_r<=hour_r;
+        min_r<=min_r;
+        sec_r<=sec_r-1;
+      end 
+    end
+  end
+end
 endmodule
